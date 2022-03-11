5c4273f7c1bfcf08eac06c27b0b3459801ef6f7d
abhay-singh-au3 <abhaysingh2402@gmail.com>
2022-03-11 20:07:11 +0530

    final testings

--- ecomm-app/src/pages/homepage.component.jsx
+++ ecomm-app/src/pages/homepage.component.jsx
@@ -4,6 +4,7 @@ import Directory from '../components/directory/directory.component';
 
 import './homepage.styles.scss';
 console.log(process.env)
+console.log('.env.sample')
 export default () => (
     <div className="homepage">
         <Directory />
