bc27cd239f27f04feed09630cc652717e6e20563
abhay-singh-au3 <abhaysingh2402@gmail.com>
2022-03-11 20:06:05 +0530

    testing pr

--- ecomm-app/src/pages/homepage.component.jsx
+++ ecomm-app/src/pages/homepage.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Directory from '../components/directory/directory.component';
 
 import './homepage.styles.scss';
-
+console.log(process.env)
 export default () => (
     <div className="homepage">
         <Directory />
